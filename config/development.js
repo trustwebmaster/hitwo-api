48d794473899447ca3e0d3df37b5212a83dfb1dd
trust musikiri <49308670+trustwebmaster@users.noreply.github.com>
2020-06-29 03:23:09 +0200

    update

--- config/development.js
+++ config/development.js
@@ -1,6 +1,7 @@
 module.exports = {
     env: 'development',
-    db: 'mongodb://localhost:27017/hitwo-database',
+    //db: 'mongodb://localhost:27017/hitwo-database',
+    db: 'mongodb+srv://ngonidzashe:2Z5gM4tEOCIaKvqv@cluster0-uirhn.mongodb.net/test?retryWrites=true&w=majority',
     port: 8080,
     message: 'Local connection'
 };
